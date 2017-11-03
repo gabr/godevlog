e35d018354e3678a03e635f95914903ab54965c0
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-11-03 13:17:53 +0100

    first - go lang dev log

---
+++ devlog.go
@@ -0,0 +1,26 @@
+/*
+
+github.com/gabr
+
+INFO q0 Learning materials in order:
+	https://www.youtube.com/watch?v=XCsL89YtqCs
+	https://golang.org/doc/code.html
+	https://tour.golang.org
+	>	https://golang.org/doc/effective_go.html	q1
+	https://www.youtube.com/watch?v=f6kdp27TYZs
+	https://www.youtube.com/watch?v=QDDwwePbDtw
+	https://golang.org/doc/codewalk/sharemem/
+	https://vimeo.com/53221558
+	https://golang.org/doc/articles/wiki/
+	https://golang.org/doc/codewalk/functions/
+
+INFO q2 Oficial language docs
+	https://golang.org/doc/
+	https://golang.org/ref/spec
+
+INFO q3 Sources of news
+	https://blog.golang.org/
+
+TODO q1 Finish reading docs
+
+*/
